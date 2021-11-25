d51bde3732646a8056c85e05a0e9f85fb0a1ceca
Johng117 <83654331+Johng117@users.noreply.github.com>
2021-11-25 16:09:52 +0000

    Added input field.

--- src/NameSorter.js
+++ src/NameSorter.js
@@ -1,20 +1,53 @@
 import Data from "./babyNamesData.json";
-import React from "react";
+import React, { useState } from "react";
 import NameOfBaby from "./NameOfBaby";
 
 const NameSorter = () => {
-  const sortedNames=Data.sort((a, b) => {
-      return (a.name>b.name ? 1 : (a.name<b.name ? -1 : 0));
+  const [textInput, setTextInput] = useState({ value: "" });
+
+  function inputHandler(e) {
+    return setTextInput({
+      ...textInput,
+      value: e.currentTarget.value,
+    });
+  }
+  console.log(textInput.value);
+
+  const sortedNames = Data.sort((a, b) => {
+    return a.name > b.name ? 1 : a.name < b.name ? -1 : 0;
   });
   return (
     <div className="name-container">
-      {sortedNames.map((element) =>
-        element.sex === "f" ? (
-          <NameOfBaby pink={element.name} />
-        ) : (
-          <NameOfBaby blue={element.name} />
-        ),
-      )}
+      <div className="form-container">
+        <form>
+          <input
+            type="text"
+            value={textInput.value}
+            onChange={inputHandler}
+          ></input>
+        </form>
+      </div>
+      <div className="names">
+        { textInput.value.length===0
+        ? sortedNames.map((element) => {
+             return (element.sex === "f" ? (
+            <NameOfBaby pink={element.name} />
+          ) : (
+            <NameOfBaby blue={element.name} />
+          ));
+          }) : null}
+
+         { (textInput.value.length!==0
+         ? (sortedNames.map((element) => {
+            return element.name.toLowerCase().includes(textInput.value) ? (
+              element.sex === "f" ? (
+                <NameOfBaby pink={element.name} />
+              ) : (
+                <NameOfBaby blue={element.name} />
+              )
+            ) : null;
+         })) : null) }
+      </div>
     </div>
   );
 };
